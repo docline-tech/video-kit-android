d8c9a632a6037a732facca5364b180698caea31e
Cristian Palos Rejano <c.palos@docline.com>
2022-04-13 14:30:27 +0200

    Initial commit. Initial version

---
+++ app/src/main/java/io/docline/sdk/videoconsultation/example/MainActivity.kt
@@ -0,0 +1,169 @@
+package io.docline.sdk.videoconsultation.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import docline.doclinevideosdk.DoclineVideocallView
+import docline.doclinevideosdk.core.listeners.ArchiveListener
+import docline.doclinevideosdk.core.listeners.ChatListener
+import docline.doclinevideosdk.core.listeners.ConnectionListener
+import docline.doclinevideosdk.core.listeners.DoclineListener
+import docline.doclinevideosdk.core.listeners.enums.CameraSource
+import docline.doclinevideosdk.core.listeners.enums.ResponseError
+import docline.doclinevideosdk.core.listeners.enums.ScreenView
+import docline.doclinevideosdk.core.listeners.enums.UserType
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var doclineVideoCall: DoclineVideocallView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // find component and init
+        doclineVideoCall = findViewById(R.id.doclineView)
+        doclineVideoCall.init(this)
+
+        // setup listeners
+        setupVideoConsultationListeners()
+
+        // join to video consultation
+        joinVideoConsultation(
+            server = your-server-url
+            code = your-code-room
+        )
+
+    }
+
+    private fun joinVideoConsultation(server: String, code: String) {
+        // prepare configuration (roomCode and serverURL)
+        val configuration = hashMapOf<String, Any>(
+            "serverURL" to server,  // server URL to join to the video consultation
+            "roomCode" to code      // code to join to the video consultation
+        )
+
+        // join to the video with the previous configuration defined
+        doclineVideoCall.join(configuration)
+    }
+
+    private fun setupVideoConsultationListeners() {
+        doclineVideoCall.generalListener = object: DoclineListener {
+
+            // invoked when user exit from video consultation
+            override fun consultationExit(userType: UserType) {
+                Log.i("SDK-Video", "consultationExit(userType: ${userType.name})")
+            }
+
+            // invoked when error trying connecting to consultation
+            override fun consultationJoinError(error: ResponseError) {
+                Log.i("SDK-Video", "consultationJoinError(error: ${error.name})")
+            }
+
+            // invoked when user has joined successfully to video consultation
+            override fun consultationJoinSuccess() {
+                Log.i("SDK-Video", "consultationJoinSuccess()")
+            }
+
+            // invoked when consultation joined successfully
+            override fun consultationJoined() {
+                Log.i("SDK-Video", "consultationJoined()")
+            }
+
+            // invoked when consultation
+            override fun consultationRejoin(userType: UserType) {
+                Log.i("SDK-Video", "consultationRejoin(userType: ${userType.name})")
+            }
+
+            // invoked when consultation finished successfully
+            override fun consultationTerminated(screenView: ScreenView) {
+                Log.i("SDK-Video", "consultationTerminated(screenView: ${screenView.name})")
+            }
+
+            // invoked when shows a new screen
+            override fun show(screenView: ScreenView) {
+                Log.i("SDK-Video", "show(screenView: ${screenView.name})")
+            }
+
+            // invoked when user switch front / back camera
+            override fun updatedCamera(screenView: ScreenView, source: CameraSource) {
+                Log.i("SDK-Video", "updatedCamera(screenView: ${screenView.name}, source: ${source.name})")
+            }
+
+            // invoked when user enabled or disabled camera
+            override fun updatedCamera(screenView: ScreenView, isEnabled: Boolean) {
+                Log.i("SDK-Video", "updatedCamera(screenView: ${screenView.name}, isEnabled: $isEnabled)")
+            }
+
+            // invoked when user enabled or disabled microphone
+            override fun updatedMicrophone(screenView: ScreenView, isEnabled: Boolean) {
+                Log.i("SDK-Video", "updatedMicrophone(screenView: ${screenView.name}, isEnabled: $isEnabled)")
+            }
+        }
+
+        doclineVideoCall.connectionListener = object: ConnectionListener {
+
+            // invoked when user reconnect to the video consultation
+            override fun consultationReconnected() {
+                Log.i("SDK-Video", "consultationReconnected()")
+            }
+
+            // invoked when user is reconnecting to the video consultation
+            override fun consultationReconnecting() {
+                Log.i("SDK-Video", "consultationReconnecting()")
+            }
+
+            // invoked when video consultation has disconnected by error
+            override fun disconnectedByError() {
+                Log.i("SDK-Video", "disconnectedByError()")
+            }
+
+            // invoked when user has pressed exit button
+            override fun userSelectExit() {
+                Log.i("SDK-Video", "userSelectExit()")
+            }
+
+            // invoked when user is trying to reconnect
+            override fun userTryReconnect() {
+                Log.i("SDK-Video", "userTryReconnect()")
+            }
+        }
+
+        doclineVideoCall.archiveListener = object: ArchiveListener {
+
+            // invoked when has been approved the screen recording
+            override fun screenRecordingApproved() {
+                Log.i("SDK-Video", "screenRecordingApproved()")
+            }
+
+            // invoked when has been denied the screen recording
+            override fun screenRecordingDenied() {
+                Log.i("SDK-Video", "screenRecordingDenied()")
+            }
+
+            // invoked when the screen recording has been finished
+            override fun screenRecordingFinished() {
+                Log.i("SDK-Video", "screenRecordingFinished()")
+            }
+
+            // invoked when the screen recording has started
+            override fun screenRecordingStarted() {
+                Log.i("SDK-Video", "screenRecordingStarted()")
+            }
+        }
+
+        doclineVideoCall.chatListener = object: ChatListener {
+
+            // invoked when user has received a chat message
+            override fun messageReceived() {
+                Log.i("SDK-Video", "messageReceived()")
+            }
+
+            // invoked when user has sent a chat message
+            override fun messageSent() {
+                Log.i("SDK-Video", "messageSent()")
+            }
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,174 @@
+# SDK Video consultation Example
+
+## Configuration:
+You need add the following lines to `settings.gradle` (root project) in order to find libraries in maven repositories:
+```
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        ...
+        maven { url 'https://nexus.docline.com/repository/maven-public/' }
+        maven { url 'https://tokbox.bintray.com/maven' }
+        maven { url 'https://jcenter.bintray.com' }
+    }
+}
+```
+Also, you need to add the following code to your `build.gradle` (app root folder):
+```
+plugins {
+    ...
+    id 'kotlin-kapt'
+}
+android {
+    ...
+    dataBinding {
+        enabled = true
+    }
+    viewBinding {
+        enabled = true
+    }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+}
+dependencies {
+    ...
+    implementation 'io.docline:video-consultation:1.0.5'
+}
+```
+In file `colors.xml`, you must define `colorPrimaryDark` color:
+```
+<resources>
+    ...
+    <color name="colorPrimaryDark">your-dark-color-here</color>
+    ...
+</resources>
+```
+
+## Simple usage:
+Add our component to your layout.xml:
+```
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_height="match_parent"
+    android:layout_width="match_parent"
+    android:orientation="vertical">
+    
+    <docline.doclinevideosdk.DoclineVideocallView
+        android:id="@+id/doclineView"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"/>
+
+</LinearLayout>
+```
+
+Then use it in your Activity or Fragment:
+```
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // find component and init
+    val doclineVideoCall = findViewById<DoclineVideocallView>(R.id.doclineView)
+    doclineVideoCall.init(this)
+    
+    // prepare configuration (roomCode and serverURL)
+    val configuration = hashMapOf<String, Any>(
+        "serverURL" to "url",   // server URL to join to the video consultation
+        "roomCode" to "ea"      // code to join to the video consultation
+    )
+
+    // join to the video with the previous configuration defined
+    doclineVideoCall.join(configuration)
+}
+```
+
+## Extended usage:
+Optionally, you can add some listeners:
+
+`DoclineListener:`
+```
+doclineVideoCall.generalListener = object: DoclineListener {
+
+    // invoked when user exit from video consultation
+    override fun consultationExit(userType: UserType) {}
+
+    // invoked when error trying connecting to consultation
+    override fun consultationJoinError(error: ResponseError) {}
+
+    // invoked when user has joined successfully to video consultation
+    override fun consultationJoinSuccess() {}
+
+    // invoked when consultation joined successfully
+    override fun consultationJoined() {}
+
+    // invoked when consultation
+    override fun consultationRejoin(userType: UserType) {}
+
+    // invoked when consultation finished successfully
+    override fun consultationTerminated(screenView: ScreenView) {}
+
+    // invoked when shows a new screen
+    override fun show(screenView: ScreenView) {}
+
+    // invoked when user switch front / back camera
+    override fun updatedCamera(screenView: ScreenView, source: CameraSource) {}
+
+    // invoked when user enabled or disabled camera
+    override fun updatedCamera(screenView: ScreenView, isEnabled: Boolean) {}
+
+    // invoked when user enabled or disabled microphone
+    override fun updatedMicrophone(screenView: ScreenView, isEnabled: Boolean) {}
+}
+```
+`ConnectionListener:`
+```
+doclineVideoCall.connectionListener = object: ConnectionListener {
+
+    // invoked when user reconnect to the video consultation
+    override fun consultationReconnected() {}
+
+    // invoked when user is reconnecting to the video consultation
+    override fun consultationReconnecting() {}
+
+    // invoked when video consultation has disconnected by error
+    override fun disconnectedByError() {}
+
+    // invoked when user has pressed exit button
+    override fun userSelectExit() {}
+
+    // invoked when user is trying to reconnect
+    override fun userTryReconnect() {}
+}
+
+```
+`ArchiveListener:`
+```
+doclineVideoCall.archiveListener = object: ArchiveListener {
+
+    // invoked when has been approved the screen recording
+    override fun screenRecordingApproved() {}
+
+    // invoked when has been denied the screen recording
+    override fun screenRecordingDenied() {}
+
+    // invoked when the screen recording has been finished
+    override fun screenRecordingFinished() {}
+
+    // invoked when the screen recording has started
+    override fun screenRecordingStarted() {}
+}
+
+```
+`ChatListener:`
+```
+doclineVideoCall.chatListener = object: ChatListener {
+
+    // invoked when user has received a chat message
+    override fun messageReceived() {}
+
+    // invoked when user has sent a chat message
+    override fun messageSent() {}
+}
+```
